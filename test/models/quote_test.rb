07e4c99e0a8380f8f5c3370448dd062cf68f7241
Joe Henriod <joehenriod@gmail.com>
2015-08-22 18:16:42 +0000

    deleted initial test

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -2,10 +2,11 @@ require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
 
-  test "unique_tag" do 
-    quote = Quote.create(:author => 'Ken Mazaika', :saying => 'You only yolo once.')
-    expected = 'KM#' + quote.id.to_s
-    actual = quote.unique_tag
+  #test "unique_tag" do 
+   # quote = Quote.create(:author => 'Ken Mazaika', :saying => 'You only yolo once.')
+    #expected = 'KM#' + quote.id.to_s
+    #actual = quote.unique_tag
+    #assert_equal expected, actual
   end
 
 end
