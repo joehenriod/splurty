ac9fedeecd9c66ec78a14c0d7bb1dac8b247ac0b
Joe Henriod <joehenriod@gmail.com>
2015-08-23 22:21:32 +0000

    bring test back

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
 
-  #test "unique_tag" do 
-   # quote = Quote.create(:author => 'Ken Mazaika', :saying => 'You only yolo once.')
-    #expected = 'KM#' + quote.id.to_s
-    #actual = quote.unique_tag
-    #assert_equal expected, actual
+  test "unique_tag" do 
+    quote = Quote.create(:author => 'Ken Mazaika', :saying => 'You only yolo once.')
+    expected = 'KM#' + quote.id.to_s
+    actual = quote.unique_tag
+   	assert_equal expected, actual
   end
 
 end
