1b938ca31abaed892dee95440bc6445fc2a7d384
Joe Henriod <joehenriod@gmail.com>
2015-10-20 19:15:49 +0000

    Tests now pass: added page not found

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -15,7 +15,10 @@ class QuotesController < ApplicationController
   end
 
   def show
-  	 @quote = Quote.find(params[:id])
+  	 @quote = Quote.where(:id => params[:id]).first
+     if @quote.blank?
+      render :text => "Not Found", :status => :not_found
+    end
   end
 
 	private
