b75c2a57957730ce8264eec15c19a37dfe47cdae
Joe Henriod <joehenriod@gmail.com>
2015-10-22 02:59:50 +0000

    fixed

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class QuoteTest < ActiveSupport::TestCase
 
   test "unique_tag" do 
-    quote = FactoryGirl.create(:quote)
+    quote = FactoryGirl.create(:quote, :author => 'Ken Mazaika')
     expected = 'KM#' + quote.id.to_s
     actual = quote.unique_tag
    	assert_equal expected, actual
