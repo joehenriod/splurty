afadb85edea6d8860e1e0cf9979393b691b1022c
Joe Henriod <joehenriod@gmail.com>
2015-10-27 20:23:40 +0000

    up2date

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,4 +17,8 @@ end
 
 class ActionController::TestCase
   include Devise::TestHelpers
+    def setup
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        sign_in FactoryGirl.create(:user)
+    end
 end
